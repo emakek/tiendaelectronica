16f9b7c160197d0c3dc24b788aee85454106cd6b
emakek <130604390+emakek@users.noreply.github.com>
2025-07-02 16:55:54 -0300

    Delete electrotienda directory

--- electrotienda/productos/forms.py
+++
@@ -1,9 +0,0 @@
-# productos/forms.py
-
-from django import forms
-from .models import MensajeContacto
-
-class ContactoForm(forms.ModelForm):
-    class Meta:
-        model = MensajeContacto
-        fields = ['nombre', 'email', 'mensaje']
